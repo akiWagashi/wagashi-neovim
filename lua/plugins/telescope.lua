37fb650ae6915596c968a4ee572ee32699543b21
akiWagashi <qq1076363319@gmail.com>
2026-04-09 16:13:54 +0800

    Add keymapping in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,5 +12,10 @@ return
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Document symbols" })
+
+        vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions, { desc = "LSP definitions" })
+        vim.keymap.set("n", "<leader>gr", builtin.lsp_references, { desc = "LSP references" })
+        vim.keymap.set("n", "<leader>gi", builtin.lsp_implementations, { desc = "LSP implementations" })
     end
 }
